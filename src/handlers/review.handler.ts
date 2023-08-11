a9d3e9835e89c5722d18606a9b5a0d23f0c28cdc
Nehuen Cisneros <cisneros.n.b@outlook.com>
2023-08-11 15:54:53 -0300

    cambios en el controller de reviews

--- src/handlers/review.handler.ts
+++ src/handlers/review.handler.ts
@@ -1,7 +1,7 @@
 import { Response, Request } from "express";
 import Movie from "../models/movie.model";
 import Review from "../models/review.model";
-import { createReview } from "../controllers/review.controller";
+import { createReview, getReviewsController } from "../controllers/review.controller";
 import mongoose from "mongoose";
 
 export const newReview = async (req: Request, res: Response) => {
@@ -15,3 +15,13 @@ export const newReview = async (req: Request, res: Response) => {
         res.status(400).json({ error: error.message });
     }
 };
+
+export const allReviews =async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const response = await getReviewsController(+id)
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
--- src/routes/review.routes.ts
+++ src/routes/review.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
-import { newReview } from '../handlers/review.handler';
+import { newReview, allReviews} from '../handlers/review.handler';
 
 const reviewsRouter: Router = Router()
+
+    reviewsRouter.get("/:id", allReviews)
 
     reviewsRouter.post("/new/:id", newReview)
 
