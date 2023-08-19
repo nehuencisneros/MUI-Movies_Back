510579ce8d87b9c9aef5ca11136503dbdfa0e716
Nehuen Cisneros <cisneros.n.b@outlook.com>
2023-08-19 00:27:15 -0300

    indentado

--- src/handlers/review.handler.ts
+++ src/handlers/review.handler.ts
@@ -1,8 +1,5 @@
 import { Response, Request } from "express";
-import Movie from "../models/movie.model";
-import Review from "../models/review.model";
 import { createReview, getReviewsController } from "../controllers/review.controller";
-import mongoose from "mongoose";
 
 export const newReview = async (req: Request, res: Response) => {
     const { id } = req.params;
